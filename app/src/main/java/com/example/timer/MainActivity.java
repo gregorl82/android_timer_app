7a718d5dd614438f0955024d1159baefd66df707
Gregor <gregorlennie@hotmail.co.uk>
2020-01-29 17:20:17 +0000

    Creates variables and assigns onClickListeners to buttons

--- app/src/main/java/com/example/timer/MainActivity.java
+++ app/src/main/java/com/example/timer/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.timer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CountDownTimer mCountDownTimer;
+    private Button mStartPauseButton;
+    private TextView mClockText;
+
+    private boolean mTimerRunning;
+
+    private long mTimeLeftInMillis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mClockText = findViewById(R.id.timerTextView);
+        mStartPauseButton = findViewById(R.id.startButton);
+
+        mStartPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mTimerRunning){
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+            }
+        });
     }
+
+    private void startTimer(){
+    }
+
+    private void pauseTimer(){}
 }
