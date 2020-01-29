6c5c25fd5b3318c2283d06e9ec4405eecd1dce4e
Gregor <gregorlennie@hotmail.co.uk>
2020-01-29 18:17:44 +0000

    Adds startTimer, pauseTimer and updateCountdownText methods

--- app/src/main/java/com/example/timer/MainActivity.java
+++ app/src/main/java/com/example/timer/MainActivity.java
@@ -8,7 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final long TIME_IN_MILLIS = 600000;
 
     private CountDownTimer mCountDownTimer;
     private Button mStartPauseButton;
@@ -16,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean mTimerRunning;
 
-    private long mTimeLeftInMillis;
+    private long mTimeLeftInMillis = TIME_IN_MILLIS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +43,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startTimer(){
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountdownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mStartPauseButton.setText("start");
+                updateCountdownText();
+                mTimerRunning = false;
+            }
+        }.start();
+        mTimerRunning = true;
+        mStartPauseButton.setText("pause");
     }
 
-    private void pauseTimer(){}
+    private void pauseTimer(){
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        mStartPauseButton.setText("start");
+    }
+
+    private void updateCountdownText(){
+        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+        String remainingTime = String.format(Locale.getDefault(),"%02d:%02d", minutes, seconds);
+        mClockText.setText(remainingTime);
+    }
 }
